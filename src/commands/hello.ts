cbea29db1c519f15a618b0d902ab166212ccce4e
Gabriele Petronella <gabriele@buildo.io>
2017-07-24 14:51:47 +0200

    Use emoji in tests

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -2,7 +2,7 @@ import { logInfo } from '../utils';
 import { prompt as ask } from 'inquirer';
 
 export default async function hello(): Promise<void> {
-  logInfo('Hello stranger!');
+  logInfo(':wave:  Hello stranger!');
   const { name } = await ask([
     {
       type: 'input',
@@ -10,5 +10,5 @@ export default async function hello(): Promise<void> {
       message: "What's your name?"
     }
   ]);
-  logInfo(`Oh, hello, ${name}!`);
+  logInfo(`Oh, nice to meet you, ${name}!`);
 }
